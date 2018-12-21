03a206486cdca2040a160ee180657f5af7bf4807
Gustavo Ghioldi <gustavoghioldi@gmail.com>
2018-12-20 22:52:33 -0300

    new colors

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ghistylus",
-    version="0.0.3",
+    version="0.1.0",
     author="Gustavo Ghioldi",
     author_email="gustavoghioldi@gmail.com",
     description="Print Stylus",
